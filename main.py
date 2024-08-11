9737609e07e8888f5ba125f2b2003932179255f8
Limour <93720049+Limour-dev@users.noreply.github.com>
2024-08-11 12:44:34 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-import keyboard
+import keyboard, time
+
+
+def showAndHideWindow():
+    from mods.m03_windows import GetForegroundWindow, ShowWindow
+    hwnd = GetForegroundWindow()
+    while True:
+        ShowWindow(hwnd, 0)
+        yield 'showAndHideWindow: 隐藏窗口'
+        ShowWindow(hwnd, 1)
+        yield 'showAndHideWindow: 显示窗口'
+
+
+showAndHideWindow = showAndHideWindow()
+
+
+def hotkey_showAndHideWindow():
+    print(time.ctime(), next(showAndHideWindow))
+
+
+if __name__ == '__main__':
+    keyboard.add_hotkey('alt+l', hotkey_showAndHideWindow)
+    keyboard.wait()
