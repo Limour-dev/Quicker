7b5eb39ebba8707ff1ddcc0cc801581b1fd9779f
Limour <93720049+Limour-dev@users.noreply.github.com>
2024-10-31 22:29:59 +0800

    Add files via upload

--- mods/m99_ai.py
+++ mods/m99_ai.py
@@ -2,6 +2,7 @@ import os
 import json
 import urllib.request
 import urllib.error
+from typing import Literal
 
 
 def post_json(url: str, data: dict, headers: dict, decoding='utf-8'):
@@ -30,10 +31,10 @@ def post_json(url: str, data: dict, headers: dict, decoding='utf-8'):
         raise e
 
 
-def AI(system: str, user: str):
+def AI(system: str, user: str, model_level: Literal[0 ,1, 2] = 2):
     response_content: str = post_json(url=QUICKER_AI_URL,
                                       data={
-                                          'model': QUICKER_AI_MODEL,
+                                          'model': QUICKER_AI_MODEL[model_level],
                                           'messages': [
                                               {'role': 'system', 'content': system},
                                               {'role': 'user', 'content': user}
@@ -49,9 +50,12 @@ def AI(system: str, user: str):
 QUICKER_AI_URL = os.getenv('QUICKER_AI_URL', 'https://api.openai.com/v1/chat/completions')
 QUICKER_AI_KEY = os.getenv('QUICKER_AI_KEY', 'sk-free')
 QUICKER_AI_MODEL = os.getenv('QUICKER_AI_MODEL', 'gpt-4o-mini')
+QUICKER_AI_MODEL = QUICKER_AI_MODEL.split('|')
+if len(QUICKER_AI_MODEL) == 1:
+    QUICKER_AI_MODEL = QUICKER_AI_MODEL * 3
+print(f'AI 0: {QUICKER_AI_MODEL[0]}', f'AI 1: {QUICKER_AI_MODEL[1]}', f'AI 2: {QUICKER_AI_MODEL[2]}')
 
 if __name__ == '__main__':
     print(QUICKER_AI_URL)
     print(QUICKER_AI_KEY)
-    print(QUICKER_AI_MODEL)
-    print(AI('response_language: zh-CN', 'hello!'))
+    print(AI('response_language: zh-CN', 'hello!', 1))
